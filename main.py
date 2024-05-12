2dd922ae9b7b95cfe8f631db50c16bea34f6f1ab
raqibcodes <abdulraqibshakir03@gmail.com>
2024-05-12 10:03:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -234,7 +234,6 @@ st.markdown(
         }
 
         .content {
-            background-color: #f5f5f5;
             padding: 20px;
             border-radius: 10px;
             box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.1);
